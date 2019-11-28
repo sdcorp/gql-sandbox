153e404e916347c3dcde32eb64330b2df26d5041
Dmitriy Soldatov <dmitrsold@gmail.com>
2019-11-28 18:44:55 +0200

    post resolver WIP

--- server/src/entity/Post.ts
+++ server/src/entity/Post.ts
@@ -1,14 +1,18 @@
-import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm';
+import { ObjectType, Field, ID } from 'type-graphql';
 
+@ObjectType()
 @Entity()
 export class Post extends BaseEntity {
+  @Field(() => ID)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field()
   @Column()
   title: string;
 
+  @Field()
   @Column()
   text: string;
 }
-
--- server/src/resolvers/PostResolver.ts
+++ server/src/resolvers/PostResolver.ts
@@ -1,12 +1,12 @@
-import { Query, Resolver, Mutation, Arg, Authorized } from 'type-graphql';
+import { Query, Resolver, Mutation, Arg } from 'type-graphql';
 import { Post } from '../entity/Post';
 
 @Resolver()
 export class PostResolver {
-  @Authorized()
-  @Query(() => String)
-  getPost() {
-    return 'my post!';
+  @Query(() => [Post])
+  async getPosts() {
+    const posts = await Post.find();
+    return posts;
   }
   @Mutation(() => Boolean)
   async create(@Arg('title') title: string, @Arg('text') text: string) {
